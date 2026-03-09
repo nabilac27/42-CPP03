0b66a2c645bde06b2106eb3e1356c42c268eaed6
Nabila Chairunnisa <nabilac@Nabilas-MacBook-Air.local>
2026-03-09 13:56:14 +0100

    ex01 constructors done

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchairun <nchairun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/09 13:50:46 by nchairun          #+#    #+#             */
+/*   Updated: 2026/03/09 13:54:18 by nchairun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+// can’t do anything if it has no hit points or energy points left
+//  ClapTrap instances should not interact directly with one another,  and the parameters will not refer to another instance of ClapTrap
+
+/* 
+    Attack costs 1 energy
+    Repair costs 1 energy
+    If hitPoints == 0 → ClapTrap can't do anything
+    If energyPoints == 0 → ClapTrap can't act
+    takeDamage() just reduces HP
+*/
+
+ClapTrap::ClapTrap() : name("default"), hitPoints(10), energyPoints(10), attackDamage(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string& name) : name(name), hitPoints(10), energyPoints(10), attackDamage(0)
+{
+	std::cout << "Parameterized constructor of " << name << " called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other)
+{
+	*this = other;
+	std::cout << "Copy constructor of " << name << " called" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor of " << name << " called" << std::endl;
+}
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: nchairun <nchairun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 13:27:13 by nchairun          #+#    #+#             */
-/*   Updated: 2026/03/09 13:28:00 by nchairun         ###   ########.fr       */
+/*   Updated: 2026/03/09 13:52:12 by nchairun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,6 @@
 #define CLAPTRAP_HPP
 
 #include <iostream>
-
-// can’t do anything if it has no hit points or energy points left
-//  ClapTrap instances should not interact directly with one another,  and the parameters will not refer to another instance of ClapTrap
-
-/* 
-    Attack costs 1 energy
-    Repair costs 1 energy
-    If hitPoints == 0 → ClapTrap can't do anything
-    If energyPoints == 0 → ClapTrap can't act
-    takeDamage() just reduces HP
-*/
 
 class ClapTrap
 {
